e033e8a7e69ed1f5363df1b270184686fb51f87a
Stevenrej <steveo732@gmail.com>
2023-04-20 23:04:07 -0500

    check added, if statement updated

--- assets/js/theme/global/show-button.js
+++ assets/js/theme/global/show-button.js
@@ -1,18 +1,22 @@
 export default async function showRemoveAllButtonIfCartHasItems() {
     const deleteAllFromCartBtn = document.querySelector('#delete-all-from-cart');
 
+    if (!deleteAllFromCartBtn) {
+        return;
+    }
+
     try {
         const cartResponse = await fetch('http://localhost:3001/api/storefront/carts?include=lineItems.physicalItems.options,lineItems.digitalItems.options');
         const cartData = await cartResponse.json();
         const cart = cartData[0];
 
-        if (cart && (cart.lineItems.physicalItems.length > 0 || cart.lineItems.digitalItems.length > 0)) {
+        if (cart && (cart.lineItems.physicalItems.length > 0 || cart.lineItems.digitalItems.length > 0 || cart.lineItems.customItems.length > 0 || cart.lineItems.giftCertificates.length > 0)) {
             deleteAllFromCartBtn.classList.remove('hidden');
         } else {
             deleteAllFromCartBtn.classList.add('hidden');
         }
     } catch (error) {
-        console.error(error);
+        // console.error('Error showing remove all button:', error);
     }
 }
 
